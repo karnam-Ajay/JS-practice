dae77083afea02e9b4e8349e5093d30f4745659a
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-07-11 08:05:34 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc";
+
+let valueInNum = Number(score);
+// console.log(typeof score);
+// console.log(valueInNum);
+// console.log(typeof valueInNum);
+
+// basic conversions
+// "33" => 33 
+// "33abc" => NaN --> type number
+// true => 1 , false => 0.
+
+let isLogged = "ajay";
+let boolIsLogged = Boolean(isLogged)
+
+console.log(typeof isLogged);
+console.log(typeof boolIsLogged);
