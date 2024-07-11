b4fe318e7a900ea96ef6a92c4234129b53a8ad85
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-07-11 07:23:15 +0000

    datatypes intro

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// primitive data types.
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not assigned.
+// symbol => for repre'tng Uniqueness.
+
+//Objects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);   // object
