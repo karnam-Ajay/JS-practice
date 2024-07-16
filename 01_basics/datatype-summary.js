c91d823bf8b586759f7e8f30b94d0e9737a46181
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-07-16 08:34:50 +0000

    stack and heap in js

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -39,3 +39,36 @@ const myFunction = function(){
 
 // return type of all Non-primitive datatypes is "object"
 // and datatype of all Non-primitive datatypes is "function"
+
+
+//------------------------------------------------------------
+
+// Stack memory(primitive datatypes stored) --> it gives copy of value.
+
+let myYoutube = 'techsavour';
+
+let anotherchnl = myYoutube;
+
+anotherchnl = 'karnamajay';  // so here copy of orginal value is given,  
+ //but original value remains same
+
+console.log(myYoutube);
+console.log(anotherchnl);
+
+// Heap memory(Non-primitive datatypes stored)--> it gives reference of 
+//original value from memory
+
+const firstUser = {
+    email:'karnam@google.com',
+    age:27
+}
+
+const secondUser = firstUser;
+
+secondUser.email = 'ajay@google.com';
+
+console.log(firstUser.email);
+console.log(secondUser.email);
+
+//In Non-primitive datatype we will be able to change the original value
+// whereas in primitive datatype we can change only copy of value.
