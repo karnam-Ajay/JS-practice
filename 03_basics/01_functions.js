8f8c44cc30526ebb248d23cf7e54270689cde28c
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-07-28 11:59:49 +0000

    passing objs and arrays to function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,3 +22,58 @@ function isUserLogged(userName){
 }
 
 // console.log(isUserLogged());
+
+
+// taking multiple values in function args
+
+function calculateItemsPrice(...total){   // rest operator
+    return total;  // puts all values in an array.
+};
+
+//  console.log(calculateItemsPrice(200,300,500)); 
+
+
+// rest operator with multiple variables.
+function calculateItemsPrice2(val1,val2,...total){   
+    return total;  // puts all values in an array.
+};
+
+//console.log(calculateItemsPrice2(200,300,500,1500));  
+
+// val1 = 200,val2 = 300, total = [500,1500];
+
+
+// Passing object in a function
+
+const demoObj = {
+    username:"Ajay",
+    price:199
+};
+
+function funObj(anyObj){
+    console.log(`username is ${anyObj.username} and price is ${anyObj.price}`);
+};
+
+// funObj(demoObj);
+
+// passing object in args
+
+// funObj(
+//     {
+//         username:"vijay",
+//         price:399
+//     }
+// );
+
+
+
+// passing array values in function.
+
+const myfuncArr = [200,400,300,500];
+
+function myfuncArray(getArr){
+    return getArr[2];
+}
+
+// console.log(myfuncArray(myfuncArr));
+//console.log(myfuncArray([600,300,200,700]));  passing directly in args.
