cad7c83ec457e46cdb46e0250acab55c54e04999
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-08-06 12:55:40 +0000

    switch,truthy concepts

---
+++ 04_Control_flow/truthy.js
@@ -0,0 +1,43 @@
+// truthy and falsy
+
+const userEmail = [];
+
+// if (userEmail) {
+//     console.log("got user");
+// }
+// else{
+//     console.log("user not logged in");
+// }
+
+// by default Falsy values
+// false, 0 , -0 , bigInt 0n, "" , null , undefined , NaN
+
+// by defaulty Truthy values
+// "0", 'false', " ", [] , function () {}
+
+
+
+// if (userEmail.length === 0) {
+//     console.log("userEmail is an empty array");
+// }
+
+// const obj = {};
+
+// if (Object.keys(obj).length === 0) {
+//     console.log("empty object");
+// }
+
+// Nullish colascing operator ??: null undefined
+
+// const val1 = 5 ?? 10;
+// const val1 = null ?? 10;  //10
+// const val1 = undefined ?? 15;
+
+// console.log(val1);
+
+// ternary operator is different from nullish colascing operator
+
+const iceCreamVal = 70;
+
+// iceCreamVal <=80 ? console.log("less than 80 "): console.log("greater than 80");
+
