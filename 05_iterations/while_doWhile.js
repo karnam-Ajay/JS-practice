bda46f8d85a9e8eb6aab412a76eb32cddd3043d8
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-08-11 08:40:14 +0000

    while and do-while loops

---
+++ 05_iterations/while_doWhile.js
@@ -0,0 +1,28 @@
+// while
+
+// print odd numbers till 20.
+let index = 1;
+while (index <= 20) {
+   // console.log(`the value of index is ${index}`);
+    index = index + 2;
+}
+
+// arrays with while loop
+
+let myArr = ["Ajay","Vijay","Soham"];
+
+let arrIndex = 0;
+while (arrIndex < myArr.length) {
+   // console.log(`the values of arr is ${myArr[arrIndex]}`);
+    arrIndex = arrIndex + 1;
+};
+
+
+// do-while : it is used where we want run our code atleast once.
+
+let score = 0;
+
+do {
+  //  console.log(`the score is ${score}`);
+    score = score+1;
+} while (score <51);
