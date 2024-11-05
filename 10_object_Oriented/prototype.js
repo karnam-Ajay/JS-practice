c689b947c88c91f2f9f9c3a8b28442640707d2bb
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-11-05 09:37:33 +0000

    prototype concept

---
+++ 10_object_Oriented/prototype.js
@@ -0,0 +1,10 @@
+       // PROTOTYPE IN JAVASCRIPT
+
+/*
+1.The general behaviour of javascript is prototype i.e., if we trying to find something it will check on heirarchial basis until it finds a null value.
+we can also call it as inherital prototype.
+
+ex: "THIS" keyword works based on this prototype concept.
+
+2.
+*/
